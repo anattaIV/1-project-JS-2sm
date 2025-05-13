d8b50716e8a7b12b800bd2654a42f9a69e40ca08
anattaIV <anattaiv@gmail.com>
2025-05-13 17:29:57 +0300

    add project 1

---
+++ scripts/index.js
@@ -0,0 +1,40 @@
+// Получаем элементы DOM
+const cardTitleInput = document.getElementById('card-title');
+const addCardButton = document.getElementById('add-card');
+const cardContainer = document.getElementById('card-container');
+
+function createCard(title) {
+  const cardDiv = document.createElement('div');
+  cardDiv.classList.add('card');
+
+  const titleElement = document.createElement('h3');
+  titleElement.textContent = title;
+  cardDiv.appendChild(titleElement);
+
+  const likeButton = document.createElement('button');
+  likeButton.textContent = 'Выполнено';
+  cardDiv.appendChild(likeButton);
+
+  const deleteButton = document.createElement('button');
+  deleteButton.textContent = 'Удалить';
+  cardDiv.appendChild(deleteButton);
+
+  likeButton.addEventListener('click', function() {
+    cardDiv.classList.toggle('liked'); // добавление класса
+  });
+
+  deleteButton.addEventListener('click', function() {
+    cardDiv.remove(); //кикает карту
+  });
+
+  return cardDiv;
+}
+
+addCardButton.addEventListener('click', function() {
+  const title = cardTitleInput.value;
+  if (title !== '') {
+    const newCard = createCard(title);
+    cardContainer.appendChild(newCard);
+  }
+});
+
